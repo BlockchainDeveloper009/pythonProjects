36c99e9057bc8fa709cac17c73dca7ae9d0182d3
BlockchainDeveloper009 <blkcdev@gmail.com>
2024-01-12 10:00:26 -0600

    push all code

---
+++ Projects/proj_pytest_playwright/proj_pytest_playwright.Readme.md
@@ -0,0 +1,22 @@
+all test should start with naming convention test_
+
+#Run playwright tests
+
+####headless;
+
+$pytest
+
+#####with head
+pytest --headed
+
+pytest --browser webkit
+
+[python_codegen](https://playwright.dev/python/docs/codegen-intro)
+
+#Running Codegen
+
+playwright codegen demo.playwright.dev/todomvc
+
+
+[Running tests](https://playwright.dev/python/docs/running-tests)
+
---
+++ Projects/proj_pytest_playwright/test_fixtures.py
@@ -0,0 +1,18 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+
+@pytest.fixture(scope="function", autouse=True)
+def before_each_after_each(page: Page):
+    print("before the test runs")
+
+    # Go to the starting url before each test.
+    page.goto("https://playwright.dev/")
+    yield
+
+    print("after the test runs")
+
+
+def test_main_navigation(page: Page):
+    # Assertions use the expect API.
+    expect(page).to_have_url("https://playwright.dev/")
